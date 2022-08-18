4aa05ad0f5cad675ff3f749850311e558681ef69
exant9 <90297180+exant9@users.noreply.github.com>
2022-08-18 15:27:35 +0700

    d

--- rclone.bat
+++ rclone.bat
@@ -129,9 +129,6 @@ if defined mkdirpath[%x%] (
 cd "C:\a_fiyandha\z-bat-vbs-file\rclone"
 
 (
-
-
-
 echo cd %%userprofile%%
 
 echo rclone cleanup "aaveusdt_sg1:/"
@@ -179,9 +176,6 @@ echo ^)
 
 
 (
-
-
-
 
 echo set selopcfolders[0]=a_fiyandha
 echo set selopcfolders[1]=3D Objects
@@ -238,16 +232,11 @@ echo ^)
 
 echo rclone moveto "aaveusdt_sg1:/Selo/main_cloud/computers/alyx/%%copyname%%" "aaveusdt_sg1:/Selo/main_cloud/computers/alyx/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_2.bat"
 
 
 
 (
-
-
-
 
 echo set selopcfolders[0]=a_fiyandha
 echo set selopcfolders[1]=3D Objects
@@ -306,17 +295,12 @@ echo ^)
 
 echo rclone moveto "renaave50_sg2:/Selo/main_cloud/computers/alyx/%%copyname%%" "renaave50_sg2:/Selo/main_cloud/computers/alyx/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_3.bat"
 
 
 
 
 (
-
-
-
 
 echo set selopcfolders[0]=a_fiyandha
 echo set selopcfolders[1]=3D Objects
@@ -373,17 +357,12 @@ echo ^)
 
 echo rclone moveto "renaave7_sg3:/Selo/main_cloud/computers/alyx/%%copyname%%" "renaave7_sg3:/Selo/main_cloud/computers/alyx/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_4.bat"
 
 
 
 
 (
-
-
-
 
 echo set selopcfolders[0]=a_fiyandha
 echo set selopcfolders[1]=3D Objects
@@ -440,17 +419,12 @@ echo ^)
 
 echo rclone moveto "oudhoh0050_sg4:/Selo/main_cloud/computers/alyx/%%copyname%%" "oudhoh0050_sg4:/Selo/main_cloud/computers/alyx/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_5.bat"
 
 
 
 
 (
-
-
-
 
 echo set selopcfolders[0]=a_fiyandha
 echo set selopcfolders[1]=3D Objects
@@ -507,17 +481,12 @@ echo ^)
 
 echo rclone moveto "nkiux77509_sg5:/Selo/main_cloud/computers/alyx/%%copyname%%" "nkiux77509_sg5:/Selo/main_cloud/computers/alyx/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_6.bat"
 
 
 
 
 (
-
-
-
 
 echo set selopcfolders[0]=a_fiyandha
 echo set selopcfolders[1]=3D Objects
@@ -574,17 +543,12 @@ echo ^)
 
 echo rclone moveto "didrhj9036_sg6:/Selo/main_cloud/computers/alyx/%%copyname%%" "didrhj9036_sg6:/Selo/main_cloud/computers/alyx/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_7.bat"
 
 
 
 
 (
-
-
-
 
 echo set selopcfolders[0]=a_fiyandha
 echo set selopcfolders[1]=3D Objects
@@ -641,17 +605,12 @@ echo ^)
 
 echo rclone moveto "hjski8292882_sg7:/Selo/main_cloud/computers/alyx/%%copyname%%" "hjski8292882_sg7:/Selo/main_cloud/computers/alyx/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_8.bat"
 
 
 
 
 (
-
-
-
 
 echo set selopcfolders[0]=a_fiyandha
 echo set selopcfolders[1]=3D Objects
@@ -708,17 +667,12 @@ echo ^)
 
 echo rclone moveto "jsnwj84738_sg8:/Selo/main_cloud/computers/alyx/%%copyname%%" "jsnwj84738_sg8:/Selo/main_cloud/computers/alyx/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_9.bat"
 
 
 
 
 (
-
-
-
 
 echo set selopcfolders[0]=a_fiyandha
 echo set selopcfolders[1]=3D Objects
@@ -775,17 +729,12 @@ echo ^)
 
 echo rclone moveto "ffdnsak8272793_sg9:/Selo/main_cloud/computers/alyx/%%copyname%%" "ffdnsak8272793_sg9:/Selo/main_cloud/computers/alyx/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_10.bat"
 
 
 
 
 (
-
-
-
 
 echo set selopcfolders[0]=a_fiyandha
 echo set selopcfolders[1]=3D Objects
@@ -842,8 +791,6 @@ echo ^)
 
 echo rclone moveto "otipes7948_sg10:/Selo/main_cloud/computers/alyx/%%copyname%%" "otipes7948_sg10:/Selo/main_cloud/computers/alyx/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_11.bat"
 
 
@@ -852,9 +799,6 @@ echo cd %%userprofile%%
 REM  Cacaaaa   _______________________________________________________
 
 (
-
-
-
 
 echo set cacapcfolders[0]=document caca
 echo set cacapcfolders[1]=com.mojang
@@ -890,16 +834,11 @@ echo ^)
 
 echo rclone moveto "jjajai2818_sg1:/Caca/main_cloud/computers/laptop_caca/%%copyname%%" "jjajai2818_sg1:/Caca/main_cloud/computers/laptop_caca/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_12.bat"
 
 
 
 (
-
-
-
 
 echo set cacapcfolders[0]=document caca
 echo set cacapcfolders[1]=com.mojang
@@ -935,16 +874,11 @@ echo ^)
 
 echo rclone moveto "eleoek28828_sg2:/Caca/main_cloud/computers/laptop_caca/%%copyname%%" "eleoek28828_sg2:/Caca/main_cloud/computers/laptop_caca/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_13.bat"
 
 
 
 (
-
-
-
 
 echo set cacapcfolders[0]=document caca
 echo set cacapcfolders[1]=com.mojang
@@ -980,17 +914,12 @@ echo ^)
 
 echo rclone moveto "wjwn91178_sg3:/Caca/main_cloud/computers/laptop_caca/%%copyname%%" "wjwn91178_sg3:/Caca/main_cloud/computers/laptop_caca/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_14.bat"
 
 
 
 
 (
-
-
-
 
 echo set cacapcfolders[0]=document caca
 echo set cacapcfolders[1]=com.mojang
@@ -1026,17 +955,12 @@ echo ^)
 
 echo rclone moveto "iaowk272818_sg4:/Caca/main_cloud/computers/laptop_caca/%%copyname%%" "iaowk272818_sg4:/Caca/main_cloud/computers/laptop_caca/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_15.bat"
 
 
 
 
 (
-
-
-
 
 echo set cacapcfolders[0]=document caca
 echo set cacapcfolders[1]=com.mojang
@@ -1072,17 +996,12 @@ echo ^)
 
 echo rclone moveto "ejsjskw28272_sg5:/Caca/main_cloud/computers/laptop_caca/%%copyname%%" "ejsjskw28272_sg5:/Caca/main_cloud/computers/laptop_caca/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_16.bat"
 
 
 
 
 (
-
-
-
 
 echo set cacapcfolders[0]=document caca
 echo set cacapcfolders[1]=com.mojang
@@ -1118,17 +1037,12 @@ echo ^)
 
 echo rclone moveto "sjsiowkw272892_sg6:/Caca/main_cloud/computers/laptop_caca/%%copyname%%" "sjsiowkw272892_sg6:/Caca/main_cloud/computers/laptop_caca/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_17.bat"
 
 
 
 
 (
-
-
-
 
 echo set cacapcfolders[0]=document caca
 echo set cacapcfolders[1]=com.mojang
@@ -1164,17 +1078,12 @@ echo ^)
 
 echo rclone moveto "s81wjwo17_sg7:/Caca/main_cloud/computers/laptop_caca/%%copyname%%" "s81wjwo17_sg7:/Caca/main_cloud/computers/laptop_caca/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_18.bat"
 
 
 
 
 (
-
-
-
 
 echo set cacapcfolders[0]=document caca
 echo set cacapcfolders[1]=com.mojang
@@ -1210,17 +1119,12 @@ echo ^)
 
 echo rclone moveto "jskwonw_sg8:/Caca/main_cloud/computers/laptop_caca/%%copyname%%" "jskwonw_sg8:/Caca/main_cloud/computers/laptop_caca/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_19.bat"
 
 
 
 
 (
-
-
-
 
 echo set cacapcfolders[0]=document caca
 echo set cacapcfolders[1]=com.mojang
@@ -1256,17 +1160,12 @@ echo ^)
 
 echo rclone moveto "usowns81_sg9:/Caca/main_cloud/computers/laptop_caca/%%copyname%%" "usowns81_sg9:/Caca/main_cloud/computers/laptop_caca/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_20.bat"
 
 
 
 
 (
-
-
-
 
 echo set cacapcfolders[0]=document caca
 echo set cacapcfolders[1]=com.mojang
@@ -1302,17 +1201,12 @@ echo ^)
 
 echo rclone moveto "9288wowj_sg10:/Caca/main_cloud/computers/laptop_caca/%%copyname%%" "9288wowj_sg10:/Caca/main_cloud/computers/laptop_caca/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_21.bat"
 
 
 
 
 (
-
-
-
 
 echo cd %%userprofile%%
 
@@ -1347,17 +1241,12 @@ echo rclone copy "xjdik82288:/rclone/gcr/fiyandhax-mipa4@sman5tambunselatan.sch.
 echo rclone copy "xjdik82288:/rclone/gcr/fiyandhax-mipa4@sman5tambunselatan.sch.id/Shared" "iajdos282728_sg9:/Selo/fiyandhax-mipa4@sman5tambunselatan.sch.id/%%copyname%%/Shared" --drive-skip-dangling-shortcuts --drive-skip-shortcuts --drive-server-side-across-configs --drive-keep-revision-forever --auto-confirm --quiet --track-renames --drive-allow-import-name-change --ignore-errors  --drive-acknowledge-abuse  --drive-stop-on-upload-limit --drive-stop-on-download-limit --ignore-size --max-transfer 740G --max-depth 999999 --cutoff-mode=cautious --low-level-retries 9999999999 --retries 10  --drive-skip-dangling-shortcuts --drive-skip-shortcuts
 echo rclone copy "xjdik82288:/rclone/gcr/fiyandhax-mipa4@sman5tambunselatan.sch.id/Shared" "iajdos282728_sg10:/Selo/fiyandhax-mipa4@sman5tambunselatan.sch.id/%%copyname%%/Shared" --drive-skip-dangling-shortcuts --drive-skip-shortcuts --drive-server-side-across-configs --drive-keep-revision-forever --auto-confirm --quiet --track-renames --drive-allow-import-name-change --ignore-errors  --drive-acknowledge-abuse  --drive-stop-on-upload-limit --drive-stop-on-download-limit --ignore-size --max-transfer 740G --max-depth 999999 --cutoff-mode=cautious --low-level-retries 9999999999 --retries 10  --drive-skip-dangling-shortcuts --drive-skip-shortcuts
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_22.bat"
 
 
 
 
 (
-
-
-
 
 echo cd %%userprofile%%
 
@@ -1423,9 +1312,6 @@ echo rclone copy "xjdik82288:/rclone/gcr/m.shahruxips-1@sman5tambunselatan.sch.i
 
 (
 
-
-
-
 echo cd %%userprofile%%
 
 echo rclone copy "fiyandha_sman5,shared_with_me:/" "fiyandha_sman5:/Shared" --drive-server-side-across-configs --drive-keep-revision-forever --auto-confirm --quiet --track-renames --drive-allow-import-name-change --ignore-errors  --drive-acknowledge-abuse  --drive-stop-on-upload-limit --drive-stop-on-download-limit --ignore-size --max-transfer 15G --max-depth 999999 --cutoff-mode=cautious --low-level-retries 9999999999 --retries 10    --drive-skip-dangling-shortcuts --drive-skip-shortcuts 
@@ -1444,9 +1330,6 @@ echo rclone move "927281jwow,shared_with_me:/jY1CJ1hE4P/main_pc/Downloads_pool" 
 
 
 (
-
-
-
 
 echo set selopcfolders[0]=a_fiyandha
 echo set selopcfolders[1]=3D Objects
@@ -1503,8 +1386,6 @@ echo ^)
 
 echo rclone moveto "9288wowj_sg10:/Selo/main_cloud/computers/alyx/%%copyname%%" "9288wowj_sg10:/Selo/main_cloud/computers/alyx/%%copyname%%_fix" --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  --low-level-retries 9999999999 --auto-confirm --quiet  --drive-allow-import-name-change --ignore-errors  
 
-echo cd %%userprofile%%
-
 )>"C:\a_fiyandha\z-bat-vbs-file\strtp-util\_25.bat"
 
 
@@ -1512,9 +1393,6 @@ echo cd %%userprofile%%
 
 
 (
-
-
-
 
 echo cd %%userprofile%%
 
