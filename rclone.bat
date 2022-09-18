8ace5f01bf8baf5dd5d504cd5d36130ed41e7758
exant9 <90297180+exant9@users.noreply.github.com>
2022-09-18 20:40:28 +0700

    S

--- rclone.bat
+++ rclone.bat
@@ -1865,13 +1865,6 @@ echo     call rclone sync "iqk818,shared_with_me:/jY1CJ1hE4P/%%%%selofldrs[%%x%%
 echo     call rclone sync "iqk818,shared_with_me:/jY1CJ1hE4P/%%%%selofldrs[%%x%%]%%%%" "iqk818,shared_with_me:/dump 2781/" --drive-keep-revision-forever --auto-confirm --drive-server-side-across-configs --ignore-errors --quiet --drive-allow-import-name-change --drive-acknowledge-abuse --drive-stop-on-upload-limit --drive-stop-on-download-limit --ignore-size --max-transfer 740G --cutoff-mode=cautious --drive-copy-shortcut-content --low-level-retries 9999999999 --retries 10 --track-renames
 echo     set /a "x+=1"
 echo     GOTO :i291111^)
-echo set "x=0"
-echo :i2911112
-echo if defined cacafldrs[%%x%%] ^(
-echo     call rclone sync "iqk818,shared_with_me:/jY1CJ1hE4P/%%%%cacafldrs[%%x%%]%%%%" "iqk818,shared_with_me:/dump 2781/" --drive-keep-revision-forever --auto-confirm --drive-server-side-across-configs --ignore-errors --quiet --drive-allow-import-name-change --drive-acknowledge-abuse --drive-stop-on-upload-limit --drive-stop-on-download-limit --ignore-size --max-transfer 740G --cutoff-mode=cautious --drive-copy-shortcut-content --low-level-retries 9999999999 --retries 10 --track-renames
-echo     call rclone sync "iqk818,shared_with_me:/jY1CJ1hE4P/%%%%cacafldrs[%%x%%]%%%%" "iqk818,shared_with_me:/dump 2781/" --drive-keep-revision-forever --auto-confirm --drive-server-side-across-configs --ignore-errors --quiet --drive-allow-import-name-change --drive-acknowledge-abuse --drive-stop-on-upload-limit --drive-stop-on-download-limit --ignore-size --max-transfer 740G --cutoff-mode=cautious --drive-copy-shortcut-content --low-level-retries 9999999999 --retries 10 --track-renames
-echo     set /a "x+=1"
-echo     GOTO :i2911112^)
 
 echo set "x=0"
 echo :i912111
